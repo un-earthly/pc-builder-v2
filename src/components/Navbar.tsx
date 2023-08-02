d75bbe5cb97c59590d315caa9a7415e3ce4245e7
MD. Alamin <vijayalamin@gmail.com>
2023-08-03 01:29:13 +0600

    deploy issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ export default function Navbar() {
                         <Link href="/pc-builder">
 
                             Pc Builder</Link>
-                    </button>
+                    </button> 
 
                     <button
                         className="duration-100 text-indigo-700  focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
